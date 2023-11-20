ba00b1b94b94158112ddddab8077795c1a2776ef
Eric Heintzmann <heintzmann.eric@free.fr>
2023-11-20 09:04:52 +0100

    Fix date format in user rout. Fix #48

--- src/main/java/com/openclassrooms/api/model/response/user/UserResponse.java
+++ src/main/java/com/openclassrooms/api/model/response/user/UserResponse.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.api.model.response.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.openclassrooms.api.model.response.Response;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,6 +23,7 @@ public class UserResponse implements Response {
     private String name;
 
     @JsonProperty(value = "created_at")
+    @JsonFormat(pattern = "yyyy/MM/dd", timezone = "UTC")
     @Schema(
             type = "string",
             example = "1970/01/01",
@@ -30,6 +32,7 @@ public class UserResponse implements Response {
     private Instant createdAt;
 
     @JsonProperty(value = "updated_at")
+    @JsonFormat(pattern = "yyyy/MM/dd", timezone = "UTC")
     @Schema(
             type = "string",
             example = "2023/02/03",
